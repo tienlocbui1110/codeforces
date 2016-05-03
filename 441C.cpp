90c67d7722ccda608b8c7f129e33a4ab1d05e92f
Tien Loc <tienlocbui1110@gmail.com>
2016-05-03 18:20:38 +0700

    hello

---
+++ 441C.cpp
@@ -0,0 +1,50 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+void xuat(int x, int y)
+{
+	cout << x << " " << y << " ";
+}
+
+int main()
+{
+	int n, m, k;
+	cin >> n >> m >> k;
+	int temp = k;
+	int x = 1; int y = 1;
+
+	while (temp-- > 1)
+	{
+		cout << 2 << " ";
+		for (int i = 1; i <= 2;i++)
+		{
+			xuat(x, y);
+			if (x % 2 != 0 && y < m)
+				y++;
+			else if (x % 2 == 0 && y > 1)
+				y--;
+			else if (x % 2 != 0 && y == m)
+				x++;
+			else if (x % 2 == 0 && y == 1)
+				x++;
+		}
+		cout << endl;
+	}
+
+	int dem = (n*m) - 2 * (k - 1);
+	cout << dem << " ";
+	for (int i = 1; i <= dem;i++)
+	{
+		xuat(x, y);
+		if (x % 2 != 0 && y < m)
+			y++;
+		else if (x % 2 == 0 && y > 1)
+			y--;
+		else if (x % 2 != 0 && y == m)
+			x++;
+		else if (x % 2 == 0 && y == 1)
+			x++;
+	}
+}
